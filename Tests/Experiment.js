625a99e3f20d7a21d8fc923c8f2be1c5b77a4fdd
Mitchellbchristensen <52010189+Mitchellbchristensen@users.noreply.github.com>
2019-07-25 15:45:43 -0600

    Update Experiment.js

--- Tests/Experiment.js
+++ Tests/Experiment.js
@@ -1,29 +1,22 @@
-//Experiment.js
-describe('Protractor Demo App', function () {
-    var firstNumber = element(by.model('first'));
-    var secondNumber = element(by.model('second'));
-    var goButton = element(by.id('gobutton'));
-    var latestResult = element(by.binding('latest'));
-    var history = element.all(by.repeater('result in memory'));
-
-    function add(a, b) {
-        firstNumber.sendKeys(a);
-        secondNumber.sendKeys(b);
-        goButton.click();
-    }
+ //experiment.js
+describe('UofU Loginpage', function () {
 
     beforeEach(function () {
-        browser.get('http://juliemr.github.io/protractor-demo/');
+        browser.get('https://healthcare.utah.edu/');
     });
 
-    it('should have a history', function () {
-        add(1, 2);
-        add(3, 4);
-
-        expect(history.count()).toEqual(2);
-
-        add(5, 6);
-
-        expect(history.count()).toEqual(0); // This is wrong!
-    });
+    it('Fail login', function () {
+        element(by.id('Login')).sendKeys('MitchellC');
+        element(by.id('password')).sendKeys('Timp1234');
+        //var guru = element(by.xpath('/section/div/div/div[1]/div[1]/div/div[2]/form/button'));
+            //element(guru.getText()).click();
+        //setTimeout(function () { alert("Hello"); }, 30000);
+        //browser.sleep(5000);
+        //browser.getallwindowhandles().then(function (handles) {
+        //    newwindowhandle = handles[1]; // this is your new window
+            //browser.switchto().window(newwindowhandle).then(function () {
+                //expect(browser.getcurrenturl()).tomatch('https://mychart.med.utah.edu/mychart/default.asp');
+            //});
+        //})
+    })
 });
