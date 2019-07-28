7c49a8a221378bef991e65425c6569728e7f0b46
Mitchell Christensen <MitchellBChristensen@gmail.com>
2019-07-28 10:48:34 -0600

    Fixing variables

--- Tests/Experiment.js
+++ Tests/Experiment.js
@@ -1,13 +1,17 @@
  //experiment.js
 describe('Experiment with different tests', function () {
+    var username = element(by.model('login'));
+    var password = element(by.id('password'));
+    var login = element(buttonText('Log In to MyChart'));
+
     beforeEach(function () {
         browser.get('');
     });
 
     it('Fail login', function () {
-        log.sendKeys(browser.params.login.user); //referencing a variable and a paramater/user (This wasn't nessesary but I wanted to show I could do it)
-        pass.sendKeys(browser.params.login.password);
-        Lb.click();
+        username.sendKeys(browser.params.login.user); //referencing a variable and a paramater/user (This wasn't nessesary but I wanted to show I could do it)
+        password.sendKeys(browser.params.login.password);
+        login.click();
         browser.getAllWindowHandles().then(function (handles) {
             newWindowHandle = handles[1];
             browser.switchTo().window(newWindowHandle).then(function () {
